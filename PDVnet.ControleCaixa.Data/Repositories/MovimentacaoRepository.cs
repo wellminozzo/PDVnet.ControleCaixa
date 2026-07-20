5609b01aca062fba9675a49a1c0fd2e9f1e1dbc5
Wellerson Minozzo <wellerson_80@hotmail.com>
2026-07-20 16:35:40 -0300

    logicas no repository para o crud no db

--- PDVnet.ControleCaixa.Data/Repositories/MovimentacaoRepository.cs
+++ PDVnet.ControleCaixa.Data/Repositories/MovimentacaoRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using PDVnet.ControleCaixa.Data.Contexts;
+using PDVnet.ControleCaixa.Model.Caixa;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,4 +8,34 @@ namespace PDVnet.ControleCaixa.Data.Repositories;
 
 public class MovimentacaoRepository
 {
+    private readonly AppDbContext _context;
+
+    public MovimentacaoRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<MovimentacaoCaixa?> GetByIdAsync(int id)
+    {
+        return await _context.MovimentacaoCaixa.FindAsync(id);
+    }
+
+    public async Task AddAsync(MovimentacaoCaixa movimentacao)
+    {
+        await _context.MovimentacaoCaixa.AddAsync(movimentacao);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task UpdateAsync(MovimentacaoCaixa movimentacao)
+    {
+        _context.MovimentacaoCaixa.Update(movimentacao);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(MovimentacaoCaixa movimentacao)
+    {
+        _context.MovimentacaoCaixa.Remove(movimentacao);
+        await _context.SaveChangesAsync();
+    }
+
 }
